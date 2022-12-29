3728cb15f03c29339ff004dde3188aa76a12b326
Jessica Desotell <jyspaun@mtu.edu>
2022-12-29 14:05:24 -0500

    automate_updates

--- flowchart.py
+++ flowchart.py
@@ -5,6 +5,7 @@ import streamlit.components.v1 as components
 import pandas as pd
 import networkx as nx
 from pyvis.network import Network
+from bs4 import BeautifulSoup
 
 
 st.title('10th Planet System')
@@ -52,27 +53,6 @@ got_net.repulsion(node_distance=420, central_gravity=0.33,
                    spring_length=110, spring_strength=0.10,
                    damping=0.95)
 
-# step = 50
-# x = -300
-# y = -250
-# legend_nodes = [
-#     (
-#         num_actual_nodes + legend_node, 
-#         {
-#             'group': legend_node, 
-#             'label': str(legend_node),
-#             'size': 30, 
-#             # 'fixed': True, # So that we can move the legend nodes around to arrange them better
-#             'physics': False, 
-#             'x': x, 
-#             'y': f'{y + legend_node*step}px',
-#             'shape': 'box', 
-#             'widthConstraint': 50, 
-#             'font': {'size': 20}
-#         }
-#     )
-#     for legend_node in range(num_legend_nodes)
-# ]
 
 # add neighbor data to node hover data
 for node in got_net.nodes:
@@ -81,46 +61,39 @@ for node in got_net.nodes:
 	node["value"] = len(neighbor_map[node["id"]])
 
 
-#got_net.show("10thPlanetSystem.html")
-got_net.save_graph('index.html')
-
-# Save and read graph as HTML file (on Streamlit Sharing)
-# try:
-# 	print('shared')
-# 	path = '/tmp'
-# 	got_net.save_graph(f'{path}/10thPlanetSystem.html')
-# 	HtmlFile = open(f'{path}/10thPlanetSystem.html','r',encoding='utf-8')
-# 	# Save and read graph as HTML file (locally)
-# except:
-# 	print('local')
-# 	path = '.'
-# 	got_net.generate_html('10thPlanetSystem.html')
-# 	HtmlFile = open('10thPlanetSystem.html','r',encoding='utf-8')
+got_net.save_graph('graphs/index.html')
 
 
-# # Load HTML into HTML component for display on Streamlit
-# #print(HtmlFile.read())
-# components.html(HtmlFile.read(), height=1000, width=10000)
+# Opening the html file
+HTMLFile = open("graphs/index.html", "r")
+
+HTMLFile_styled = open('System_Flows/index_v2.html', 'r')
+  
+# Reading the file
+index = HTMLFile.read()
+index_styled = HTMLFile_styled.read()
+  
+# Creating a BeautifulSoup object and specifying the parser
+S = BeautifulSoup(index, 'html.parser')
+SS = BeautifulSoup(index_styled, 'html.parser')
+  
+
+graph_html = S.find_all('script')[-1]
+SS.find_all('script')[-1].replace_with(graph_html)
+
+with open("index.html", "w") as file:
+    file.write(str(SS))
+
+print('Full Graph Complete')
 
 
-
-
-print(system_dict)
+print('Starting Sub Graphs')
 for key in system_dict.keys():
-	print(key)
-	st.title(key)
 	got_net = Network(height="100%",width='100%', bgcolor="black", font_color="white", select_menu=False)
 
-	# set the physics layout of the network
-	# type_dict = {'Positional':'dot', 'Transition':'diamond','Submission':'star','Sweep':'square','Escape':'triangle'} 
-	# system_dict = {'Lockdown':'red','Open Guard':'blue','Mount':'white','Rubber Guard':'purple','Bottom Mount':'teal', 'Back Mount':'gray','Truck':'gold', 'Side Mount':'green', 'Spiderweb':'fuschsia','X Guard':'cyan','Leg Entanglements':'salmon','Reverse Spiderweb':'orange'}
-	# got_data = pd.read_csv("moves_flow.csv")
-	# technique_db = pd.read_csv('technique_db.csv')
-	# technique_db = technique_db.set_index('Technique')
 
 	sub_tech_db = technique_db[technique_db['System'] == key]
-	sub_tech_db['Source']= sub_tech_db.index
-	# got_data['Source'] = got_data.index
+	sub_tech_db.loc[:,'Source']= list(sub_tech_db.index)
 
 
 	sub_data = pd.merge(got_data, sub_tech_db, how='right')
@@ -164,29 +137,7 @@ for key in system_dict.keys():
 	                   spring_length=110, spring_strength=0.10,
 	                   damping=0.95)
 
-	# step = 50
-	# x = -300
-	# y = -250
-	# legend_nodes = [
-	#     (
-	#         num_actual_nodes + legend_node, 
-	#         {
-	#             'group': legend_node, 
-	#             'label': str(legend_node),
-	#             'size': 30, 
-	#             # 'fixed': True, # So that we can move the legend nodes around to arrange them better
-	#             'physics': False, 
-	#             'x': x, 
-	#             'y': f'{y + legend_node*step}px',
-	#             'shape': 'box', 
-	#             'widthConstraint': 50, 
-	#             'font': {'size': 20}
-	#         }
-	#     )
-	#     for legend_node in range(num_legend_nodes)
-	# ]
 
-	# add neighbor data to node hover data
 	for node in got_net.nodes:
 
 		node["title"] += " Neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
@@ -194,4 +145,31 @@ for key in system_dict.keys():
 
 
 	#got_net.show("10thPlanetSystem.html")
-	got_net.save_graph(key+'.html')
+	got_net.save_graph('graphs/'+key+'.html')
+
+	# Opening the html file
+	HTMLFile = open('graphs/'+key+'.html', "r")
+
+	HTMLFile_styled = open('Styled/'+key+'.html', 'r')
+	  
+	# Reading the file
+	index = HTMLFile.read()
+	index_styled = HTMLFile_styled.read()
+	  
+	# Creating a BeautifulSoup object and specifying the parser
+	S = BeautifulSoup(index, 'html.parser')
+	SS = BeautifulSoup(index_styled, 'html.parser')
+	  
+
+	graph_html = S.find_all('script')[-1]
+	SS.find_all('script')[-1].replace_with(graph_html)
+
+	with open('System_Flows/'+key+".html", "w") as file:
+	    file.write(str(SS))
+
+print('Sub Graphs Complete')
+
+
+
+
+
